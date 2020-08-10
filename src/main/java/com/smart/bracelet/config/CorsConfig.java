04dfbd3de5b9b0d5a57f920c9814a8d2a5d4229b
jiahaowang <2328135501@qq.com>
2020-08-10 14:29:16 +0800

    跨域

---
+++ src/main/java/com/smart/bracelet/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.smart.bracelet.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * TODO:
+ *
+ * @author fushibo
+ * @version 1.0
+ * @date 2020-06-18 8:05
+ */
+public class CorsConfig implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse) response;
+        res.addHeader("Access-Control-Allow-Credentials", "true");
+        res.addHeader("Access-Control-Allow-Origin", "*");
+        res.addHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        res.addHeader("Access-Control-Allow-Headers", "Content-Type,X-CAF-Authorization-Token,sessionToken,X-TOKEN");
+        if (((HttpServletRequest) request).getMethod().equals(HttpMethod.OPTIONS)) {
+            response.getWriter().println(HttpStatus.OK);
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
