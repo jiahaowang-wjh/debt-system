3f67a64129d34fcd39c5075d07521d86b094b926
527842695@qq.com <527842695@qq.com>
2020-09-14 19:30:41 +0800

    电子章

---
+++ src/main/java/com/smart/bracelet/model/doc/BusEletronUser.java
@@ -0,0 +1,63 @@
+package com.smart.bracelet.model.po.doc;
+
+import java.io.Serializable;
+
+/**
+ * bus_eletron_user
+ * @author
+ */
+public class BusEletronUser implements Serializable {
+    /**
+     * 个人帐号ID
+     */
+    private String acctId;
+
+    /**
+     * 姓名
+     */
+    private String userName;
+
+    /**
+     * 身份证号
+     */
+    private String userCard;
+
+    /**
+     * 电话
+     */
+    private String userTel;
+
+    private static final long serialVersionUID = 1L;
+
+    public String getAcctId() {
+        return acctId;
+    }
+
+    public void setAcctId(String acctId) {
+        this.acctId = acctId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserCard() {
+        return userCard;
+    }
+
+    public void setUserCard(String userCard) {
+        this.userCard = userCard;
+    }
+
+    public String getUserTel() {
+        return userTel;
+    }
+
+    public void setUserTel(String userTel) {
+        this.userTel = userTel;
+    }
+}
