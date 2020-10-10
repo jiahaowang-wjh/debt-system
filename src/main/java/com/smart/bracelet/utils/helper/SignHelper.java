41defa37bb762a89e7d82b93ff16a1ebe980bfcc
527842695@qq.com <527842695@qq.com>
2020-10-10 18:26:34 +0800

    签章位置调整

--- src/main/java/com/smart/bracelet/utils/helper/SignHelper.java
+++ src/main/java/com/smart/bracelet/utils/helper/SignHelper.java
@@ -586,12 +586,12 @@ public class SignHelper {
 			orgInfo = OrgInfo.ELEC_AA;
 			// 签署方信息
 			// 平台方
-			PosBeanInfo posBean1 = new PosBeanInfo("7", 443F, 148F); // 签署位置
+			PosBeanInfo posBean1 = new PosBeanInfo("8", 463F, 298F); // 签署位置
 			SignfieldInfo signfield1 = new SignfieldInfo(true, "2", fileId, null, null, null, posBean1, null,orgInfo.getSealId()); // 签署区
 			SignerAccount signAccount1 = new SignerAccount(orgInfo.getAccountId(), orgInfo.getOrgid());//签署方账号
 			Signer signer1 = new Signer(false, 1, signAccount1, Lists.newArrayList(signfield1), null); // 签署方
 			// 用户方1
-			PosBeanInfo posBean2 = new PosBeanInfo("7", 138F, 126F); // 签署位置
+			PosBeanInfo posBean2 = new PosBeanInfo("8", 148F, 296F); // 签署位置
 			SignfieldInfo signfield2 = new SignfieldInfo(true, null, fileId, null, null, null, posBean2, null,null); // 签署区
 			SignerAccount signAccount = new SignerAccount(copierAccountId, signerAccountId);//签署方账号
 			Signer signer2 = new Signer(false, 1, signAccount, Lists.newArrayList(signfield2), null); // 签署方
@@ -611,12 +611,12 @@ public class SignHelper {
 			orgInfo = OrgInfo.ELEC_AA;
 			// 签署方信息
 			// 平台方
-			PosBeanInfo posBean1 = new PosBeanInfo("1", 110F, 271F); // 签署位置
+			PosBeanInfo posBean1 = new PosBeanInfo("1", 110F, 171F); // 签署位置
 			SignfieldInfo signfield1 = new SignfieldInfo(true, "2", fileId, null, null, null, posBean1, null,orgInfo.getSealId()); // 签署区
 			SignerAccount signAccount1 = new SignerAccount(orgInfo.getAccountId(), orgInfo.getOrgid());//签署方账号
 			Signer signer1 = new Signer(false, 1, signAccount1, Lists.newArrayList(signfield1), null); // 签署方
 			// 用户方1
-			PosBeanInfo posBean2 = new PosBeanInfo("1", 392F, 277F); // 签署位置
+			PosBeanInfo posBean2 = new PosBeanInfo("1", 392F, 177F); // 签署位置
 			SignfieldInfo signfield2 = new SignfieldInfo(true, null, fileId, null, null, null, posBean2, null,null); // 签署区
 			SignerAccount signAccount = new SignerAccount(copierAccountId, signerAccountId);//签署方账号
 			Signer signer2 = new Signer(false, 1, signAccount, Lists.newArrayList(signfield2), null); // 签署方
@@ -627,7 +627,7 @@ public class SignHelper {
 			orgInfo = OrgInfo.ELEC_AA;
 			// 签署方信息
 			// 平台方
-			PosBeanInfo posBean1 = new PosBeanInfo("1", 357F, 716F); // 签署位置
+			PosBeanInfo posBean1 = new PosBeanInfo("2", 357F, 150F); // 签署位置
 			SignfieldInfo signfield1 = new SignfieldInfo(true, "2", fileId, null, null, null, posBean1, null,orgInfo.getSealId()); // 签署区
 			SignerAccount signAccount1 = new SignerAccount(orgInfo.getAccountId(), orgInfo.getOrgid());//签署方账号
 			Signer signer1 = new Signer(false, 1, signAccount1, Lists.newArrayList(signfield1), null); // 签署方
